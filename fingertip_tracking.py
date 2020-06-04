d275849883f1eb98539da31fe2477c55f4052e65
Viktor Bubanja <viktor.bubanja@hotmail.com>
2020-06-04 19:54:42 +1200

    Add documentation

--- fingertip_tracking.py
+++ fingertip_tracking.py
@@ -1,119 +1,50 @@
+"""Module responsible for finding the fingertips within a binary image."""
+
 import cv2
 import math
-
-from dataclasses import dataclass
-
-DEFECT_THRESHOLD = 10000
-THUMB_THRESHOLD = 100
-MIN_CONTOUR_AREA = 20000
-GROUPING_Y = 30
-GROUPING_X = 30
+from group_points import group_fingertip_points
 
 
-def find_fingertip(frame, hand_region):
-    fingertips = []
-    convex_defects = []
+def find_fingertips(frame, hand_region):
+    """Find fingertips by finding a convex hull of the user's hand, grouping hull points
+    so there is one hull point per finger, find the number of significant convexity defects (X),
+    then identify the topmost X + 1 hull points as fingertips."""
     if hand_region is not None:
-        drawable_hull = cv2.convexHull(hand_region)
-        cv2.drawContours(frame, [drawable_hull], -1, (255, 0, 0), 2)
-        formatted = [tuple(i[0]) for i in drawable_hull]
-        # Sort convex hull points by their y-value.
-        print(formatted)
-        group_distance = 40  # hand_width / 5
-        fused_hull = sorted(fuse(frame, formatted, group_distance), key=lambda point: point[1])
+        hull_points = cv2.convexHull(hand_region)
+        hand_height = max(hull_points, key=lambda p: p[1])[1] - min(hull_points, key=lambda p: p[1])[1]
+        defect_threshold = int(hand_height / 4)
+        num_defects = find_number_convex_defects(hand_region, defect_threshold)
+        cv2.drawContours(frame, [hull_points], -1, (255, 0, 0), 2)
+        hull_points = [tuple(i[0]) for i in hull_points]
+        fused_hull = sorted(group_fingertip_points(hull_points), key=lambda p: p[1])
 
-        hull = cv2.convexHull(hand_region, returnPoints=False)
-        defects = cv2.convexityDefects(hand_region, hull)
-        if defects is not None:
-            hand_height = max(formatted, key=lambda x: x[1])[1] - min(formatted, key=lambda x: x[1])[1]
-            defect_threshold = int(hand_height / 4)
-            for i in range(defects.shape[0]):
-                s, e, f, d = defects[i][0]
-                start = tuple(hand_region[s][0])
-                end = tuple(hand_region[e][0])
-                far = tuple(hand_region[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                # Use cosine theorem to calculate angle between convex defect and surrounding hull points
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
-                if angle <= math.pi / 2 and d > defect_threshold:
-                    convex_defects.append(far)
-
-        convex_defects.sort(key=lambda defect: defect[1])
-        convex_defects = convex_defects[:5]
-        for defect in convex_defects:
-            cv2.circle(frame, defect, 5, [0, 255, 0], -1)
-
-        for i in range(len(convex_defects) + 1):
+        for i in range(num_defects + 1):
             if i < len(fused_hull):
                 point = (int(fused_hull[i][0]), int(fused_hull[i][1]))
-                fingertips.append(point)
+                yield point
 
-    return fingertips
+def find_number_convex_defects(hand_region, defect_threshold):
+    """Find the number of significant convexity defects in a hand (corresponding to
+    the gaps between fingers."""
+    num_defects = 0
+    hull = cv2.convexHull(hand_region, returnPoints=False)
+    defects = cv2.convexityDefects(hand_region, hull)
+    if defects is not None:
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i][0]
+            start = tuple(hand_region[s][0])
+            end = tuple(hand_region[e][0])
+            far = tuple(hand_region[f][0])
+            angle = find_angle_between_three_points(start, far, end)
+            # check angle to avoid defects that are not between two fingers
+            if angle <= math.pi / 2 and d > defect_threshold:
+                num_defects += 1
 
+    return min(num_defects, 4)
 
-def horizontal_distance(p1, p2):
-    return abs(p1[0] - p2[0])
-
-
-def vertical_distance(p1, p2):
-    return abs(p1[1] - p2[1])
-
-
-def euclidean_distance(p1, p2):
-    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-
-
-def fuse(frame, points, max_distance):
-    fused_points = []
-    length = len(points)
-    taken = [False] * length
-    for i in range(length):
-        if not taken[i]:
-            count = 1
-            point = [points[i][0], points[i][1]]
-            taken[i] = True
-            for j in range(i + 1, length):
-                p1, p2 = points[i], points[j]
-                if euclidean_distance(p1, p2) < max_distance:
-                    point[0] += points[j][0]
-                    point[1] += points[j][1]
-                    count += 1
-                    taken[j] = True
-            point[0] /= count
-            point[1] /= count
-            flagged_point = FlaggedPoint(point=(int(point[0]), int(point[1])), flag=True)
-            fused_points.append(flagged_point)
-
-
-    # for point in fused_points:
-    #     cv2.circle(frame, point.point, 10, [255, 255, 255], -1)
-
-    max_x_distance = 20
-    max_y_distance = 30
-
-    for i in range(len(fused_points) - 1):
-        for j in range(i + 1, len(fused_points)):
-            p1, p2 = fused_points[i].point, fused_points[j].point
-            if (horizontal_distance(p1, p2) < max_x_distance and
-                vertical_distance(p1, p2) < max_y_distance and
-                    fused_points[i].flag is True and
-                    fused_points[j].flag is True):
-                if p1[1] > p2[1]:
-                    fused_points[i].flag = False
-                else:
-                    fused_points[j].flag = False
-
-    # for point in fused_points:
-    #     if point.flag is True:
-    #         cv2.circle(frame, point.point, 5, [10, 30, 30], -1)
-
-    return [point.point for point in fused_points if point.flag is True]
-
-
-@dataclass
-class FlaggedPoint:
-    point: tuple
-    flag: bool
-
+def find_angle_between_three_points(p1, p2, p3):
+    a = math.sqrt((p3[0] - p1[0]) ** 2 + (p3[1] - p1[1]) ** 2)
+    b = math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+    c = math.sqrt((p3[0] - p2[0]) ** 2 + (p3[1] - p2[1]) ** 2)
+    # Use cosine theorem to calculate angle between convex defect and surrounding hull points
+    return math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))
--- main.py
+++ main.py
@@ -1,37 +1,16 @@
-import time
+"""Main module for fingertip detection and tracking project."""
 
+import time
 import cv2
 
-from fingertip_tracking import find_fingertip
+from fingertip_tracking import find_fingertips
 from hand_segmentation import find_hand_contour, run_avg, draw_hist_rectangles, hand_histogram
 
-convex_defects = []
 
 def rescale_frame(frame, wpercent=130, hpercent=130):
     width = int(frame.shape[1] * wpercent / 100)
     height = int(frame.shape[0] * hpercent / 100)
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
-
-
-def grayscale_blur_image(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return cv2.GaussianBlur(gray, (7, 7), 0)
-
-
-def main2():
-    cap = cv2.VideoCapture(0)
-
-    while cv2.waitKey(1) & 0xFF != ord('z'):
-        _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        frame = draw_hist_rectangles(frame)
-        cv2.imshow("Retrieve histogram", rescale_frame(frame))
-
-    hand_hist = hand_histogram(frame)
-
-    while cv2.waitKey(1) & 0xFF != ord('q'):
-        _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
 
 def main():
     background = None
@@ -60,8 +39,9 @@ def main():
 
         if frame_count < num_frames:
             # Note: background will be None for the first iteration.
-            gray_background = grayscale_blur_image(frame)
-            background = run_avg(gray_background, background, alpha)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)
+            background = run_avg(gray, background, alpha)
             frame_count += 1
             cv2.imshow("Calibrating background", frame)
 
@@ -69,13 +49,12 @@ def main():
             cv2.destroyWindow("Calibrating background")
             hand_region = find_hand_contour(frame, hand_hist, background)
 
-            fingertips = find_fingertip(frame, hand_region)
-            fingertips.sort(key=lambda x: x[0])
+            fingertips = find_fingertips(frame, hand_region)
+            fingertips.sort(key=lambda p: p[0])
             for finger in fingertips:
                 cv2.circle(frame, finger, 5, [255, 255, 255], -1)
 
             cv2.imshow("Live Feed", rescale_frame(frame))
-
 
     cv2.destroyAllWindows()
     cap.release()
